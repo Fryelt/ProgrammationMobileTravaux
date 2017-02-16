e28947aa88c789849f5c778df104ad98294e7b0a
Bajamin Potvin <bajamin_28@hotmail.com>
2017-02-15 23:47:05 -0500

    Revert "Exercise1"
    This reverts commit 14e873f0b02d43b4e18815f1aa451374e350ceb6.

--- js.js
+++
@@ -1,7 +0,0 @@
-function table(){
-	var numero = document.getElementById("t").value;
-	var multiples = document.querySelectorAll("LI");
-	for (var i = 0; i < 9; i++){
-		multiples[i].innerHTML = numero + " * " + (i + 1) + " = " + (numero*(i + 1));
-	}
-}
